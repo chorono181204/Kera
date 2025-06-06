bcefb5f75933aafb4a38f01bf6ce54cc95cb3ffb
chorono181204 <vndt181204@gmail.com>
2025-06-07 00:58:01 +0700

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
+import path from 'path'
+import { resolve } from 'path'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,6 +28,13 @@ export default defineConfig({
         plugins: ['@emotion/babel-plugin'],
       },
     }),
-    svgr()
-  ]
+    svgr(),
+    resolve({
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components')
+      },
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json','mjs'],
+    }),
+  ],
 })
