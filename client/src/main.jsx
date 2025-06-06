873dfffca4d913ed03eb7b70b036c90106afbc01
chorono181204 <vndt181204@gmail.com>
2025-06-07 01:58:17 +0700

    Remove motion-utils completely and update lockfile

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { Experimental_CssVarsProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 
 
+
 import theme from './theme'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
